4d9376a25f59cada89e0d076d234668dc9a65c52
Timothy Belmore <timothybelmore@trinitybiblecollege.edu>
2020-09-09 23:17:31 -0600

    Start and answer buttons created. Answer buttons hidden until Start is clicked. Also hides Start.

--- assets/script.js
+++ assets/script.js
@@ -1,27 +1,74 @@
-function QuestionAnswer(question, answers, correct) {
+class QuestionAnswer {
+    constructor(question, answers, correctAnswer) {
 
-    this.question = question;
-    this.answers = answers;
-    this.correct = correct;
+        this.question = question;
+        this.answers = answers;
+        this.correctAnswer = correctAnswer;
 
+    }
 }
 
-let question1 = new QuestionAnswer("Inside which HTML element do you put the JavaScript link?", "'<java>', '<script>', '<js>', '<javaScript>'", "<script>")
+let question1 = new QuestionAnswer("Inside which HTML element do you put the JavaScript link?", "<script>")
 
-let question2 = new QuestionAnswer("How would you write 'Hello World!' in an alert box ", "", "alert('Hello World!')" ,"")
+let question2 = new QuestionAnswer("How would you write 'Hello World!' in an alert box ", "alert('Hello World!')")
 
-let question3 = new QuestionAnswer("What is this type of function called called? function ()", "", "Both A and C")
+let question3 = new QuestionAnswer("What is this type of function called called? function ()", "Both A and C")
 
-let question4 = new QuestionAnswer("What are these examples of?", "'click','mouseover', 'resize'" , "Event Listeners")
+let question4 = new QuestionAnswer("What are these examples of?; 'click','mouseover', 'resize'", "Event Listeners")
 
-let question5 = new QuestionAnswer("What types of values can you put into an array", "", "yes")
+let question5 = new QuestionAnswer("What types of values can you put into an array", "yes")
 
-let question6 = new QuestionAnswer("What will the following code return: Boolean(10 > 9)", "", "true")
+let question6 = new QuestionAnswer("What will the following code return: Boolean(10 > 9)", "true")
 
-let question7 = new QuestionAnswer("What is the correct way to write a JavaScript array?", "", "")
+let question7 = new QuestionAnswer("What is the correct way to write a JavaScript array?"["array1 = 1, 2, a, b;", "let var = [1, 2, a, b];"], "let var = [1, 2, a, b];")
 
-let question8 = new QuestionAnswer("How would you call a function named 'myFunction'?", "", "'myFunction()'")
+let question8 = new QuestionAnswer("How would you call a function named 'myFunction'?", "'myFunction()'")
 
-let question9 = new QuestionAnswer("How would you grab the first occurrence of an HTML Element '<p>' in the document? ", "", "'document.querySelector('p')")
+let question9 = new QuestionAnswer("How would you grab the first occurrence of an HTML Element '<p>' in the document? ", "'document.querySelector('p')")
 
-let question10 = new QuestionAnswer("What JavaScript input is wrapped in quotations?", "", "string")
+let question10 = new QuestionAnswer("What JavaScript input is wrapped in quotations?", "string")
+
+$(document).ready(function () {
+    $(".hide").hide()
+    //Create .on("click", function (){}) on start button to show answer buttons
+
+    $("#start-button").on("click", function () {
+
+        $("#start-button").hide()
+        $(".hide").show()
+
+    })
+
+
+    // Create if/else statement to decide whether or not the answer selected was correct; 
+    this.isCorrectAnswer = function (userAnswer) {
+        if (userAnswer === this.correctAnswer) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+
+
+    //Create a timer
+
+
+
+
+
+
+    // if answer is incorrect subtract time from timeleft
+
+
+
+
+
+    //
+
+
+
+    //When quiz is completed either by timer reaching zero, or all questions are answered link to highscore screen
+
+
+})
