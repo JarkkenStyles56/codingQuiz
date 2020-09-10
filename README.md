3ad0f0ba1b8865dc5a4a794c7b0a166273f433a2
Timothy Belmore <timothybelmore@trinitybiblecollege.edu>
2020-09-10 00:58:53 -0600

    included all possible answers in this.answers property. Added High Scores page with submission form.

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # codingQuiz
 
+Objective of this Homework is to make create a web application in the form of a multiple choice quiz about JavaScript.
+In order to do this, I decide to use a class constructor
--- assets/script.js
+++ assets/script.js
@@ -8,25 +8,25 @@ class QuestionAnswer {
     }
 }
 
-let question1 = new QuestionAnswer("Inside which HTML element do you put the JavaScript link?", "<script>")
+let question1 = new QuestionAnswer("Inside which HTML element do you put the JavaScript link?", ["<head>", "<div>", "<li>", "<script>"], "<script>")
 
-let question2 = new QuestionAnswer("How would you write 'Hello World!' in an alert box ", "alert('Hello World!')")
+let question2 = new QuestionAnswer("How would you write 'Hello World!' in an alert box ", ["alertBox('Hello World!')", "prompt('Hello World!')", "alert('Hello World!')", "helloWorld()"], "alert('Hello World!')")
 
-let question3 = new QuestionAnswer("What is this type of function called called? function ()", "Both A and C")
+let question3 = new QuestionAnswer("What is this type of function called called? function ()", ["An Anonymous Function", "Even listener Function", "A Callback", "Both A and C"], "Both A and C")
 
-let question4 = new QuestionAnswer("What are these examples of?; 'click','mouseover', 'resize'", "Event Listeners")
+let question4 = new QuestionAnswer("What are these examples of?; 'click','mouseover', 'resize'", ["Event Listeners", "Methods", "Elements", "Commands"], "Event Listeners")
 
-let question5 = new QuestionAnswer("What types of values can you put into an array", "yes")
+let question5 = new QuestionAnswer("What are the only types of values can you put into an array", ["Strings", "Numbers", "Arrays", "yes"], "yes")
 
-let question6 = new QuestionAnswer("What will the following code return: Boolean(10 > 9)", "true")
+let question6 = new QuestionAnswer("What will the following code return: Boolean(10 > 9)", ["Syntax Error", "True", "False", "1"], "True")
 
-let question7 = new QuestionAnswer("What is the correct way to write a JavaScript array?"["array1 = 1, 2, a, b;", "let var = [1, 2, a, b];"], "let var = [1, 2, a, b];")
+let question7 = new QuestionAnswer("What is the correct way to write a JavaScript array?"["array1 = 1, 2, a, b;", "let var = [1, 2, a, b];", "array(1,2,a,b)", "None of the Above"], "let var = [1, 2, a, b];")
 
-let question8 = new QuestionAnswer("How would you call a function named 'myFunction'?", "'myFunction()'")
+let question8 = new QuestionAnswer("How would you call a function named 'myFunction'?", ["myFunction()", "thisFunction(myFunction)", "let myFunction = 'myFunction'", "Dial (693)862-8466 on a phone"], "myFunction()")
 
-let question9 = new QuestionAnswer("How would you grab the first occurrence of an HTML Element '<p>' in the document? ", "'document.querySelector('p')")
+let question9 = new QuestionAnswer("How would you grab the first occurrence of an HTML Element '<p>' in the document using javascript? ", ["With your Hand", "Ctrl+p", "document.querySelectorAll('p')", "document.querySelector('p')"], "document.querySelector('p')")
 
-let question10 = new QuestionAnswer("What JavaScript input is wrapped in quotations?", "string")
+let question10 = new QuestionAnswer("What JavaScript input is wrapped in quotations?", ["number", "if/else", "string", "boolean"], "string")
 
 
 //Create a timer
@@ -50,6 +50,7 @@ $(document).ready(function () {
 
                 if (secondsLeft === 0) {
                     clearInterval(timerInterval);
+                    
                 }
                 console.log(secondsLeft)
 
@@ -58,7 +59,7 @@ $(document).ready(function () {
         setTime()
         console.log(secondsLeft)
     })
-
+    // $("#answer-button").data(this.answers[i])
 
     // Create if/else statement to decide whether or not the answer selected was correct; 
     this.isCorrectAnswer = function (userAnswer) {
